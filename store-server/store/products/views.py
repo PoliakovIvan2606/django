8023a1924b28b1588db18e736fc3bf3f01275b8f
79964 <poliakov1324if@mail.ru>
2023-07-12 22:08:45 +0300

    new_1

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 
 def index(request):
     context = {
         'title':'Store',
     }
-    return render(request, 'products/index.html',context)
+    return render(request, 'products/index.html', context)
 
 def products(request):
     context = {
         'title':'Store-продукты',
+        'products': Product.objects.all(),
+        'products_category': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,13 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
